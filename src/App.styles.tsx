f02ddd9050651c12f8dbd21dec47c9a8c8eadf6d
SnehalKalmegh-Marta <snehal@marta.de>
2026-05-07 14:53:35 +0200

    Initial project changes

---
+++ src/App.styles.tsx
@@ -0,0 +1,7 @@
+export const AppStyles= {
+    scaleDiv1: 'transition duration-2000 ease-in-out hover:scale-x-120 hover:scale-y-110 ',
+    scaleDownDiv1: 'transition duration-2000 ease-in-out scale-x-80',
+    scaleDiv2: 'transition duration-2000 ease-in-out hover:scale-x-120 hover:scale-y-110 ',
+    scaleDownDiv2: 'transition duration-2000 ease-in-out scale-x-80',
+    imageFit: 'mb-6'
+}
